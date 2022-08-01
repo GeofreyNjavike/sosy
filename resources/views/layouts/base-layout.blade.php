700436f7a54c3cac11203bf6633b5e6b7bd4ee26
GeofreyNjavike <geofreynjavike.com>
2022-08-01 23:04:13 +0300

    forth commit

--- resources/views/layouts/base-layout.blade.php
+++ resources/views/layouts/base-layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Nova Bootstrap Template - Index</title>
+    <title>Sosy Tax & Financial Services</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -41,4 +41,3 @@
 </head>
 
 </html>
-
