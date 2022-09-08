41eed3d100152f5bf988934a0b05ed5abcd734e4
GeofreyNjavike <geofreynjavike.com>
2022-09-08 05:05:37 +0300

    7 commit

--- resources/views/editServices.blade.php
+++ resources/views/editServices.blade.php
@@ -46,7 +46,7 @@
                             @method('POST')
                             <div class="mb-6">
                                 <label for="serviceName"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Service
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Services
                                     Name</label>
                                 <input type="text" id="username-success" name="serviceName"
                                     class="bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400"
