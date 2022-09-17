5abef944d75f8c3f9220f210b8a1fc06c14dfab5
GeofreyNjavike <geofreynjavike.com>
2022-09-17 18:25:26 +0300

    sept chet

--- resources/views/components/back-end/service-list.blade.php
+++ resources/views/components/back-end/service-list.blade.php
@@ -15,7 +15,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white">
-                    @foreach ($list as $service)
+                    @forelse ($list as $service)
                         <tr class="text-gray-700">
                             <td class="px-4 py-3 border">
                                 <div class="flex items-center text-sm">
@@ -41,9 +41,30 @@
                                         class="fa fa-pencil text-green-500"></i></a>
                             </td>
                         </tr>
-                    @endforeach
 
+                    @empty
+                        <tr class="text-gray-700">
+                            <td class="px-4 py-3 border">
+                                <div class="flex items-center text-sm">
+                                    <div class="relative w-8 h-8 mr-3 rounded-full md:block">
+                                        <img class="object-cover w-full h-full rounded-full"
+                                            src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                                            alt="" loading="lazy" />
+                                        <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true">
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p class="font-semibold text-black">Not Availabe</p>
 
+                                    </div>
+                                </div>
+                            </td>
+
+                            <td class="px-4 py-3 text-ms font-semibold border">
+                                Not Available
+                            </td>
+                        </tr>
+                    @endforelse
 
 
                 </tbody>
