2e108d95f2363a11cdc005c3754baafe41d5ed32
GeofreyNjavike <geofreynjavike.com>
2022-09-17 12:49:57 +0300

    initial releas3sss3s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Scripts -->
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
     <link rel="stylesheet" href="{{ secure_asset('resouces/css/app.css') }}">
